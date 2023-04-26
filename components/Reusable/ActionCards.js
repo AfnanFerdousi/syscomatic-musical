48c30b9b4be65556d8c3524cbf97259d067b0ac1
AfnanFerdousi <afnanferdousi550@gmail.com>
2023-04-26 18:33:48 +0600

    bg changed

--- components/Reusable/ActionCards.js
+++ components/Reusable/ActionCards.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 const ActionCards = ({ action }) => {
   return (
-    <div className='bg-[#000000] rounded-md text-white font-normal text-[16px] relative
+    <div className='bg-[#1E1E1E] rounded-md text-white font-normal text-[16px] relative
     before:w-full before:h-full before:scale-x-[1.01] before:scale-y-[1.04] before:absolute before:top-[50%] before:left-[50%] 
     before:-z-10 before:translate-x-[-50%] before:translate-y-[-50%] 
     before:from-[#2DF1E6] before:to-[#A660C3] before:bg-gradient-to-br
--- components/Subscription.js
+++ components/Subscription.js
@@ -4,8 +4,8 @@ import LinearGradBtn from './Reusable/LinearGradBtn';
 
 const Subscription = () => {
     return (
-        <div className='px-20 mt-32 mx-auto' style={{ backgroundImage: `url(https://i.ibb.co/QQYP6fr/girl.png)`, backgroundRepeat: 'no-repeat', height: '400px', backgroundPosition: "cover" }}>
-           <div className='p-5 pt-[5rem] flex flex-col justify-center'>
+        <div className='mx-20 mt-32 mx-auto' style={{ backgroundImage: `url(https://i.ibb.co/QQYP6fr/girl.png)`, backgroundRepeat: 'no-repeat', height: '400px', backgroundPosition: "cover" }}>
+           <div className='pl-12 pt-[5rem] flex flex-col justify-center '>
            <h2 className='text-2xl text-[#fff]'>Get started</h2>
             <p className='mt-4 mb-6'>Join the growing list of creatives who get<br/> their visions heard today</p>
             <LinearGradBtn text="Sign up now" />
